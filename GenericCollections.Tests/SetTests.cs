42c9e27437db709ae07bdc94fbcc45a630582cf1
Akim <akim.lyubchenko@gmail.com>
2018-08-11 18:26:22 +0300

    Add xmlComments, Tests

---
+++ GenericCollections.Tests/SetTests.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace GenericCollections.Tests
+{
+    [TestFixture]
+    class SetTests
+    {
+        [Test]
+        public void Set_Add()
+        {
+            Set<int> actual = new Set<int>(new int[] { 5 });
+            int[] arr = new int[] { 3, 1, 7, 9 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                actual.Add(arr[i]);
+            }
+
+            IEnumerable<int> expected = new int[] { 1, 3, 5, 7, 9 };
+
+            CollectionAssert.AreEqual(expected, actual.ShowSet());
+        }
+
+        [Test]
+        public void Set_Clear()
+        {
+            Set<int> actual = new Set<int>(new int[] { 5, 6, 7 });
+
+            IEnumerable<int> expected = new int[] { };
+            actual.Clear();
+
+            CollectionAssert.AreEqual(expected, actual.ShowSet());
+        }
+
+        [Test]
+        public void Set_Contains()
+        {
+            Set<int> actual = new Set<int>(new int[] { 5 });
+            int[] arr = new int[] { 3, 1, 7, 9 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                actual.Add(arr[i]);
+            }
+
+            int expected = 9;
+
+            Assert.True(actual.Contains(expected));
+        }
+
+        [Test]
+        public void Set_CopyTo()
+        {
+            int arrind = 5;
+            Set<int> actual = new Set<int>(new int[] { 5, 3, 1, 7, 9 });
+
+            int[] actualArr = new int[12];
+            actual.CopyTo(actualArr, arrind);
+
+            CollectionAssert.AreEqual(new int[] { 0, 0, 0, 0, 0, 1, 3, 5, 7, 9, 0, 0 }, actualArr);
+        }
+
+        [Test]
+        public void Set_GetEnumerator()
+        {
+            Set<int> actual = new Set<int>(new int[] { 5, 6, 7, 8, 2, 9 });
+
+            IEnumerable<int> other = new int[] { 2, 5, 6, 7, 8, 9 };
+
+            CollectionAssert.AreEqual(other, actual.ShowSet());
+        }
+
+        [Test]
+        public void Set_IntersectWith()
+        {
+            Set<int> actual = new Set<int>(new int[] { 5, 6, 7, 8, 2, 9 });
+
+            IEnumerable<int> other = new int[] { 2, 12, 6, 4, 0, 9 };
+            actual.IntersectWith(other);
+
+            CollectionAssert.AreEqual(new int[] { 2, 6, 9 }, actual.ShowSet());
+        }
+
+        [Test]
+        public void Set_IsProperSubsetOf_True()
+        {
+            Set<int> actual = new Set<int>(new int[] { 5, 6, 7 });
+
+            IEnumerable<int> other = new int[] { 12, 5, 6, 7, 3, 4, 14 };
+
+            Assert.True(actual.IsProperSubsetOf(other));
+        }
+
+        [Test]
+        public void Set_IsProperSubsetOf_False()
+        {
+            Set<int> actual = new Set<int>(new int[] { 5, 6, 7, 123 });
+
+            IEnumerable<int> other = new int[] { 3, 5, 6, 7, 3, 4, 5, 3 };
+
+            Assert.False(actual.IsProperSubsetOf(other));
+        }
+
+        [Test]
+        public void Set_IsProperSupersetOf_True()
+        {
+            Set<int> actual = new Set<int>(new int[] { 12, 5, 6, 7, 3, 4, 13 });
+
+            IEnumerable<int> other = new int[] { 5, 6, 7 };
+
+            Assert.True(actual.IsProperSupersetOf(other));
+        }
+
+        [Test]
+        public void Set_IsProperSupersetOf_False()
+        {
+            Set<int> actual = new Set<int>(new int[] { 12, 5, 6, 7, 3, 4, 13 });
+
+            IEnumerable<int> other = new int[] { 5, 6, 7, 55 };
+
+            Assert.False(actual.IsProperSubsetOf(other));
+        }
+
+        [Test]
+        public void Set_IsSubsetOf_True()
+        {
+            Set<int> actual = new Set<int>(new int[] { 12, 5, 6, 7, 3, 4, 13 });
+
+            IEnumerable<int> other = new int[] { 5, 6, 7 };
+
+            Assert.True(actual.IsSubsetOf(other));
+        }
+
+        [Test]
+        public void Set_IsSubsetOf_True_Equals()
+        {
+            Set<int> actual = new Set<int>(new int[] { 5, 6, 7 });
+
+            IEnumerable<int> other = new int[] { 5, 6, 7 };
+
+            Assert.True(actual.IsSubsetOf(other));
+        }
+
+        [Test]
+        public void Set_IsSubsetOf_False()
+        {
+            Set<int> actual = new Set<int>(new int[] { 12, 5, 6, 7, 3, 4, 13 });
+
+            IEnumerable<int> other = new int[] { 5, 6, 7, 55 };
+
+            Assert.False(actual.IsSubsetOf(other));
+        }
+
+        [Test]
+        public void Set_IsSupersetOf_True()
+        {
+            Set<int> actual = new Set<int>(new int[] { 12, 5, 6, 7, 3, 4, 13 });
+
+            IEnumerable<int> other = new int[] { 5, 6, 7 };
+
+            Assert.True(actual.IsSupersetOf(other));
+        }
+
+        [Test]
+        public void Set_IsSupersetOf_True_Equals()
+        {
+            Set<int> actual = new Set<int>(new int[] { 5, 6, 7 });
+
+            IEnumerable<int> other = new int[] { 5, 6, 7 };
+
+            Assert.True(actual.IsSupersetOf(other));
+        }
+
+        [Test]
+        public void Set_IsSupersetOf_False()
+        {
+            Set<int> actual = new Set<int>(new int[] { 12, 5, 6, 7, 3, 4, 13 });
+
+            IEnumerable<int> other = new int[] { 5, 6, 7, 55 };
+
+            Assert.False(actual.IsSupersetOf(other));
+        }
+
+
+        [Test]
+        public void Set_OverLaps_True()
+        {
+            Set<int> actual = new Set<int>(new int[] { 12, 5, 6, 7, 3, 4, 13 });
+
+            IEnumerable<int> other = new int[] { 1, 2, 67, 78, 13 };
+
+            Assert.True(actual.Overlaps(other));
+        }
+
+        [Test]
+        public void Set_OverLaps_Fasle()
+        {
+            Set<int> actual = new Set<int>(new int[] { 12, 5, 6, 7, 3, 4, 13 });
+
+            IEnumerable<int> other = new int[] { 1, 2, 67, 78 };
+
+            Assert.False(actual.IsSupersetOf(other));
+        }
+
+        [Test]
+        public void Set_Remove()
+        {
+            Set<int> actual = new Set<int>(new int[] { 5, 6, 7, 8, 2, 9 });
+            actual.Remove(8);
+            actual.Remove(9);
+            actual.Remove(2);
+
+            CollectionAssert.AreEqual(new int[] { 5, 6, 7 }, actual.ShowSet());
+        }
+
+        [Test]
+        public void Set_SetEquals()
+        {
+            Set<int> actual = new Set<int>(new int[] { 5, 6, 7, 8, 2, 9 });
+
+            IEnumerable<int> other = new Set<int>(new int[] { 5, 6, 7, 8, 2, 9 });
+
+            CollectionAssert.AreEqual(other, actual.ShowSet());
+        }
+
+        [Test]
+        public void Set_SymmetricExceptWith()
+        {
+            Set<int> actual = new Set<int>(new int[] { 5, 6, 7, 8, 2, 9 });
+
+            IEnumerable<int> other = new Set<int>(new int[] { 5, 6, 14, 13 });
+            actual.SymmetricExceptWith(other);
+
+            CollectionAssert.AreEqual(new int[] { 2, 7, 8, 9, 13, 14 }, actual.ShowSet());
+        }
+
+        [Test]
+        public void Set_UnionWith()
+        {
+            Set<int> actual = new Set<int>(new int[] { 5, 6, 7, 8, 2, 9 });
+
+            IEnumerable<int> other = new Set<int>(new int[] { 5, 6, 14, 13 });
+            actual.UnionWith(other);
+
+            CollectionAssert.AreEqual(new int[] { 2, 5, 6, 7, 8, 9, 13, 14 }, actual.ShowSet());
+        }
+    }
+}
--- GenericCollections/Set.cs
+++ GenericCollections/Set.cs
@@ -4,34 +4,80 @@ using System.Collections.Generic;
 
 namespace GenericCollections
 {
-    class Set<T> : ISet<T>
+    /// <summary>
+    /// For working width sets
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <seealso cref="System.Collections.Generic.ISet{T}" />
+    public class Set<T> : ISet<T>
     {
+        #region Fields
         private BinarySearchTree.BinarySearchTree<T> set;
-
+        #endregion
+        #region Constructors        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Set{T}"/> class.
+        /// </summary>
         public Set()
         {
             set = new BinarySearchTree.BinarySearchTree<T>();
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Set{T}"/> class.
+        /// </summary>
+        /// <param name="collection">The collection.</param>
         public Set(ICollection<T> collection) : base()
         {
             InputIEnumerableValidator(collection);
 
             set = new BinarySearchTree.BinarySearchTree<T>(collection);
         }
+        #endregion
+        #region puublic API        
+        /// <summary>
+        /// Gets the number of elements contained in the <see cref="T:System.Collections.Generic.ICollection`1" />.
+        /// </summary>
         public int Count { get { return set.Count; } }
 
+        /// <summary>
+        /// Gets a value indicating whether the <see cref="T:System.Collections.Generic.ICollection`1" /> is read-only.
+        /// </summary>
         public bool IsReadOnly => false;
 
+        /// <summary>
+        /// Adds an element to the current set and returns a value to indicate if the element was successfully added.
+        /// </summary>
+        /// <param name="item">The element to add to the set.</param>
+        /// <returns>
+        ///   <see langword="true" /> if the element is added to the set; <see langword="false" /> if the element is already in the set.
+        /// </returns>
         public bool Add(T item)
             => set.Add(item);
 
+        /// <summary>
+        /// Removes all items from the <see cref="T:System.Collections.Generic.ICollection`1" />.
+        /// </summary>
         public void Clear()
             => set = new BinarySearchTree.BinarySearchTree<T>();
 
+        /// <summary>
+        /// Determines whether the <see cref="T:System.Collections.Generic.ICollection`1" /> contains a specific value.
+        /// </summary>
+        /// <param name="item">The object to locate in the <see cref="T:System.Collections.Generic.ICollection`1" />.</param>
+        /// <returns>
+        ///   <see langword="true" /> if <paramref name="item" /> is found in the <see cref="T:System.Collections.Generic.ICollection`1" />; otherwise, <see langword="false" />.
+        /// </returns>
         public bool Contains(T item)
             => set.Contains(item);
 
+        /// <summary>
+        /// Copies the elements of the <see cref="T:System.Collections.Generic.ICollection`1" /> to an <see cref="T:System.Array" />, starting at a particular <see cref="T:System.Array" /> index.
+        /// </summary>
+        /// <param name="array">The one-dimensional <see cref="T:System.Array" /> that is the destination of the elements copied from <see cref="T:System.Collections.Generic.ICollection`1" />. The <see cref="T:System.Array" /> must have zero-based indexing.</param>
+        /// <param name="arrayIndex">The zero-based index in <paramref name="array" /> at which copying begins.</param>
+        /// <exception cref="ArgumentOutOfRangeException">ArrayIndex must be more than 0</exception>
+        /// <exception cref="ArgumentException">Array length must be more than set lenght</exception>
         public void CopyTo(T[] array, int arrayIndex)
         {
             InputIEnumerableValidator(array);
@@ -56,6 +102,10 @@ namespace GenericCollections
             }
         }
 
+        /// <summary>
+        /// Removes all elements in the specified collection from the current set.
+        /// </summary>
+        /// <param name="other">The collection of items to remove from the set.</param>
         public void ExceptWith(IEnumerable<T> other)
         {
             InputIEnumerableValidator(other);
@@ -66,6 +116,12 @@ namespace GenericCollections
             }
         }
 
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>
+        /// An enumerator that can be used to iterate through the collection.
+        /// </returns>
         public IEnumerator<T> GetEnumerator()
         {
             foreach (var item in set.InOrder())
@@ -74,6 +130,10 @@ namespace GenericCollections
             }
         }
 
+        /// <summary>
+        /// Modifies the current set so that it contains only elements that are also in a specified collection.
+        /// </summary>
+        /// <param name="other">The collection to compare to the current set.</param>
         public void IntersectWith(IEnumerable<T> other)
         {
             InputIEnumerableValidator(other);
@@ -91,49 +151,44 @@ namespace GenericCollections
             }
         }
 
+        /// <summary>
+        /// Determines whether the current set is a proper (strict) subset of a specified collection.
+        /// </summary>
+        /// <param name="other">The collection to compare to the current set.</param>
+        /// <returns>
+        ///   <see langword="true" /> if the current set is a proper subset of <paramref name="other" />; otherwise, <see langword="false" />.
+        /// </returns>
         public bool IsProperSubsetOf(IEnumerable<T> other)
         {
             InputIEnumerableValidator(other);
 
-            int counter = 0;
-            foreach (var item in other)
-            {
-                counter++;
-                if (!set.Contains(item))
-                {
-                    return false;
-                }
-            }
-
-            if (counter == Count)
-            {
-                return false;
-            }
-
-            return true;
+            return IsSetOf(new Set<T>((ICollection<T>)other));
         }
 
+        /// <summary>
+        /// Determines whether the current set is a proper (strict) superset of a specified collection.
+        /// </summary>
+        /// <param name="other">The collection to compare to the current set.</param>
+        /// <returns>
+        ///   <see langword="true" /> if the current set is a proper superset of <paramref name="other" />; otherwise, <see langword="false" />.
+        /// </returns>
         public bool IsProperSupersetOf(IEnumerable<T> other)
         {
             InputIEnumerableValidator(other);
 
-            var otherSet = new BinarySearchTree.BinarySearchTree<T>(other);
-            if (otherSet.Count == set.Count)
-            {
-                return false;
-            }
+             
+            var otherSet = new Set<T>((ICollection<T>)other);
 
-            foreach (var item in set.PreOrder())
-            {
-                if (!otherSet.Contains(item))
-                {
-                    return false;
-                }
-            }
-
-            return true;
+            return otherSet.IsSetOf(this);
         }
 
+        /// <summary>
+        /// Determines whether a set is a subset of a specified collection.
+        /// </summary>
+        /// <param name="other">The collection to compare to the current set.</param>
+        /// <returns>
+        ///   <see langword="true" /> if the current set is a subset of <paramref name="other" />; otherwise, <see langword="false" />.
+        /// </returns>
         public bool IsSubsetOf(IEnumerable<T> other)
         {
             InputIEnumerableValidator(other);
@@ -149,15 +204,22 @@ namespace GenericCollections
             return true;
         }
 
+        /// <summary>
+        /// Determines whether the current set is a superset of a specified collection.
+        /// </summary>
+        /// <param name="other">The collection to compare to the current set.</param>
+        /// <returns>
+        ///   <see langword="true" /> if the current set is a superset of <paramref name="other" />; otherwise, <see langword="false" />.
+        /// </returns>
         public bool IsSupersetOf(IEnumerable<T> other)
         {
             InputIEnumerableValidator(other);
 
             var otherSet = new BinarySearchTree.BinarySearchTree<T>(other);
 
-            foreach (var item in set.PreOrder())
+            foreach (var item in otherSet.PreOrder())
             {
-                if (!otherSet.Contains(item))
+                if (!set.Contains(item))
                 {
                     return false;
                 }
@@ -166,6 +228,13 @@ namespace GenericCollections
             return true;
         }
 
+        /// <summary>
+        /// Determines whether the current set overlaps with the specified collection.
+        /// </summary>
+        /// <param name="other">The collection to compare to the current set.</param>
+        /// <returns>
+        ///   <see langword="true" /> if the current set and <paramref name="other" /> share at least one common element; otherwise, <see langword="false" />.
+        /// </returns>
         public bool Overlaps(IEnumerable<T> other)
         {
             InputIEnumerableValidator(other);
@@ -181,9 +250,23 @@ namespace GenericCollections
             return false;
         }
 
+        /// <summary>
+        /// Removes the first occurrence of a specific object from the <see cref="T:System.Collections.Generic.ICollection`1" />.
+        /// </summary>
+        /// <param name="item">The object to remove from the <see cref="T:System.Collections.Generic.ICollection`1" />.</param>
+        /// <returns>
+        ///   <see langword="true" /> if <paramref name="item" /> was successfully removed from the <see cref="T:System.Collections.Generic.ICollection`1" />; otherwise, <see langword="false" />. This method also returns <see langword="false" /> if <paramref name="item" /> is not found in the original <see cref="T:System.Collections.Generic.ICollection`1" />.
+        /// </returns>
         public bool Remove(T item)
             => set.Remove(item);
 
+        /// <summary>
+        /// Determines whether the current set and the specified collection contain the same elements.
+        /// </summary>
+        /// <param name="other">The collection to compare to the current set.</param>
+        /// <returns>
+        ///   <see langword="true" /> if the current set is equal to <paramref name="other" />; otherwise, false.
+        /// </returns>
         public bool SetEquals(IEnumerable<T> other)
         {
             InputIEnumerableValidator(other);
@@ -205,13 +288,20 @@ namespace GenericCollections
             return false;
         }
 
+        /// <summary>
+        /// Modifies the current set so that it contains only elements that are present either in the current set or in the specified collection, but not both.
+        /// </summary>
+        /// <param name="other">The collection to compare to the current set.</param>
         public void SymmetricExceptWith(IEnumerable<T> other)
         {
             InputIEnumerableValidator(other);
 
             foreach (var item in other)
             {
-                set.Remove(item);
+                if (!set.Remove(item))
+                {
+                    set.Add(item);
+                } 
             }
         }
 
@@ -221,19 +311,34 @@ namespace GenericCollections
 
             foreach (var item in other)
             {
-                if (!set.Contains(item))
-                {
                     set.Add(item);
-                }
             }
         }
 
+        /// <summary>
+        /// Adds an item to the <see cref="T:System.Collections.Generic.ICollection`1" />.
+        /// </summary>
+        /// <param name="item">The object to add to the <see cref="T:System.Collections.Generic.ICollection`1" />.</param>
         void ICollection<T>.Add(T item)
             => Add(item);
 
+        /// <summary>
+        /// Returns an enumerator that iterates through a collection.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="T:System.Collections.IEnumerator" /> object that can be used to iterate through the collection.
+        /// </returns>
         IEnumerator IEnumerable.GetEnumerator()
             => GetEnumerator();
 
+        /// <summary>
+        /// Shows the set.
+        /// </summary>
+        /// <returns> Set </returns>
+        public IEnumerable<T> ShowSet()
+            => set.ShowTree();
+        #endregion
+        #region private methods
         private void InputIEnumerableValidator(IEnumerable<T> other)
         {
             if (other == null)
@@ -241,5 +346,24 @@ namespace GenericCollections
                 throw new ArgumentNullException($"Fill {nameof(other)}");
             }
         }
+
+        private bool IsSetOf(Set<T> otherSet)
+        {
+            if (otherSet.Count == Count)
+            {
+                return false;
+            }
+
+            foreach (var item in set.InOrder())
+            {
+                if (!otherSet.Contains(item))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        #endregion
     }
 }
